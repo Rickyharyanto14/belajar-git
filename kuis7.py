d6b9f65f6b0206693dedeebabd4eafcd0568c826
Ricky Haryanto <111473502+Rickyharyanto14@users.noreply.github.com>
2023-01-01 22:35:43 +0700

    Upload file python

---
+++ kuis7.py
@@ -0,0 +1,10 @@
+class Orang:
+    def __init__(self, nama, asal):
+        self.nama = nama
+        self.asal = asal
+
+    def perkenalan(self):
+        print(f"perkenalan nama saya {self.nama} dari {self.asal}")
+    
+andi = Orang('Andi','Surabaya')
+andi.perkenalan()
